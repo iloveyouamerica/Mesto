78e3eacd42f30fca73976539ff3c5323dbf58512
Alexander <iloveyouamerica@yandex.ru>
2023-02-06 22:52:59 +0300

    feat: class UserInfo

---
+++ scripts/UserInfo.js
@@ -0,0 +1,25 @@
+
+// Принимает в конструктор объект с селекторами двух элементов: 
+// элемента имени пользователя и элемента информации о себе
+
+export class UserInfo {
+  constructor({userNameSelector, userInfoSelector}) {
+    this._userName = document.querySelector(userNameSelector);
+    this._userInfo = document.querySelector(userInfoSelector);
+  }
+
+  // публичный метод, возвращает объект с данными пользователя
+  // Этот метод пригодится когда данные пользователя нужно будет подставить в форму при открытии.
+  getUserInfo() {
+    return {
+      userName: this._userName.textContent,
+      userInfo: this._userInfo.textContent
+    };
+  }
+
+  // публичный метод setUserInfo, который принимает новые данные пользователя и добавляет их на страницу
+  setUserInfo(userData) {
+    this._userName.textContent = userData.name;
+    this._userInfo.textContent = userData.info;
+  }
+}
